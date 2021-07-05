d65ec44cac53e71237312bf7a54b72a5917c0c6d
limh <mh@minghua.li>
2021-07-05 15:04:35 +0800

    clear-all.sh

--- trail/0.clear-all.sh
+++ trail/0.clear-all.sh
@@ -9,7 +9,9 @@ echo $ROOT
 sudo rm -rf ~/.m2/repository/io/xserverless
 sudo rm -rf ~/.m2/repository/org/csource
 
-sudo rm -rf $ROOT/compose/*
+sudo rm -rf $ROOT/xserverless/trail/compose/*
+
+cd $ROOT/xserverless
 
 git reset HEAD --hard
 
